988cc83354c1db16daab2252986b1b2be08beb80
vishalkumawatvishi-ctrl <vishalkumawatvishi@gmail.com>
2025-12-20 01:15:09 +0530

    Update infoCommands.py

--- cogs/infoCommands.py
+++ cogs/infoCommands.py
@@ -17,7 +17,7 @@ CONFIG_FILE = "info_channels.json"
 class InfoCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api_url = "http://raw.thug4ff.com/info"
+        self.api_url = "http://raw.thug4ff.com/info?uid=305000592&key=test"
         self.generate_url = "http://profile.thug4ff.com/api/profile"
         self.session = aiohttp.ClientSession()
         self.config_data = self.load_config()
